f85fe653a70e74e2555db9ea3b60dcd48550ffdd
lixinrui706 <xli21@mail.csuchico.edu>
2018-03-01 10:00:49 -0800

    test

--- main.cpp
+++ main.cpp
@@ -1,11 +1,35 @@
 #include "SecretDoor/SecretDoor.h"
 #include <iostream>
-
+//Author Xinrui Li
 using namespace std;
 
 int main()
 {
   SecretDoor game;
-    
+  
+  int time;
+  int wintime = 0;
+
+  cout<<"How many times want to play?";
+  cin>>time;
+ 
+  for (int i = 0;i < wintime;i++)
+  {
+     game.newGame();
+     game.guessDoorC();
+     game.guessDoorC();
+    if(game.isWinner() == true)
+    {
+    wintime++;
+    }
+  } 
+  if(wintime<=1)
+  {
+    cout<<"You won "<<wintime<<" time"<<endl;
+  }
+  else
+  {
+    cout<<"You won "<<wintime<<" times"<<endl;
+  }
   return 0;
 }
